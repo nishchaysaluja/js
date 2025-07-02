ea4fbf90e3c5e4a35be0e33c4f81aa973a546e3d
nishchaysaluja <kbsaluja@gmail.com>
2025-07-02 09:28:09 +0000

    container config done

--- basics/variables.js
+++ basics/variables.js
@@ -1,7 +1,7 @@
-const accountId
+const accountId=144553
 let accountEmail="nishchay@gmail.com"
 var accountPassword='12345'
 accountCity="Jaipur"
 
 // accountId=2
-console.log(accountId); 
+console.log(accountId);  
